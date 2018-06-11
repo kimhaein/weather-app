270785614f034ac900cb485f3059a0072031f3b8
김혜인 <kendy93@sbcn.co.kr>
2018-06-11 13:41:35 +0900

    [완료] 날씨api 붙이기 완료

--- App.js
+++ App.js
@@ -2,17 +2,19 @@ import React , {Component} from 'react';
 import { StyleSheet, Text, View, StatusBar} from 'react-native';
 import Weather from './component/Weather.js'
 
+const API_KEY = '2d6e3515cca55a8bebe690cc2f87731c'
+
 export default class App extends Component {
   state = {
     isLoaded:false,
-    error:null
+    error:null,
+    temperature:null,
+    name:null,
   }
   componentDidMount(){
     navigator.geolocation.getCurrentPosition(  // 위치정보 확인
-      positon => {
-        this.setState({
-          isLoaded: true
-        })
+      position => {
+        this._getWeather(position.coords.latitude,position.coords.longitude)
       },
       error => {
         this.setState({
@@ -20,14 +22,27 @@ export default class App extends Component {
         })
       }
     )
+  }
+  _getWeather = (lat,long) => {
+      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
+      .then( response => response.json())
+      .then(json => {
+        console.log(json)
+        this.setState({
+          temperature : json.main.temp,
+          name:json.weather[0].main,
+          isLoaded:true,
+        })
+      }
 
+      )
   }
   render() {
-    const { isLoaded,error } = this.state
+    const { isLoaded,error,temperature, name } = this.state
     return (
       <View style={styles.container}>
       <StatusBar hidden={true}/>
-        {isLoaded ? (<Weather/>)
+        {isLoaded ? (<Weather WeatherName={name} temp={Math.ceil(temperature-273.15)}/>)
         : (<View style={styles.loading}>
             <Text style={styles.loadingText}>날씨 정보를 가져오는 중입니다...</Text>
             {error ? <Text style={styles.errorText}>{error}</Text> : null}
@@ -36,6 +51,8 @@ export default class App extends Component {
       </View>
     );
   }
+
+
 }
 
 const styles = StyleSheet.create({
--- component/Weather.js
+++ component/Weather.js
@@ -1,31 +1,81 @@
 import React , {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {LinearGradient} from 'expo';
-import {Ionicons} from '@expo/vector-icons/';
+import {MaterialCommunityIcons} from '@expo/vector-icons/';
 
-export default class Weather extends Component {
-    render(){
-        return (
-            <LinearGradient 
-            colors={['#00C6Fb','#005BEA']} 
-            style={styles.container}>
-                <View style={styles.upper}>
-                    <Ionicons
-                        color='#FFFFFF'
-                        size={144}
-                        name='ios-rainy'
-                    />
-                    <Text style={styles.temp}>35도</Text>
-                </View>
-                <View style={styles.lower}>
-                    <Text style={styles.title}>비온다</Text>
-                    <Text style={styles.subTitle}>우산챙기쇼</Text>
-                </View>
-            </LinearGradient>
-        )
+const weatherCases = {
+    Rain: {
+        colors:['#00C6FB','#005BEA'],
+        title:'비가 옵니다',
+        subTitle:'우산을 챙기세요!!',
+        icon:'weather-pouring'
+    },
+    Clear: {
+        colors:['#FEF253','#FF7300'],
+        title:'날씨 맑음',
+        subTitle:'소풍가기 좋은 날',
+        icon:'weather-sunny'
+    },
+    Thunderstorm: {
+        colors:['#00ECBC','#007ADF'],
+        title:'천둥 번개가 칩니다',
+        subTitle:'번개 조심하시요',
+        icon:'weather-lightning'
+    },
+    Clouds: {
+        colors:['#D7D2CC','#304352'],
+        title:'구름 많음',
+        subTitle:'우중충한 날씨다',
+        icon:'weather-cloudy'
+    },
+    Snow: {
+        colors:['#7DE2FC','#B9B6ES'],
+        title:'하늘에서 눈이 와요~',
+        subTitle:'눈사람을 만들어봐요',
+        icon:'weather-snowy'
+    },
+    Drizzle: {
+        colors:['#89F7FE','#66A6FF'],
+        title:'이슬이슬 이슬비',
+        subTitle:'우산을 챙겨야 되나...?',
+        icon:'weather-rainy'
+    },
+    Haze: {
+        colors:['#89F7FE','#66A6FF'],
+        title:'안개 자욱',
+        subTitle:'운전 조심하세요:)',
+        icon:'weather-fog'
+    },
+    Mist: {
+        colors:['#89F7FE','#66A6FF'],
+        title:'안개 자욱',
+        subTitle:'운전 조심하세요:)',
+        icon:'weather-fog'
     }
-
 }
+function Weather({temp,WeatherName}){
+    console.log(WeatherName)
+    return (
+        <LinearGradient 
+        colors={weatherCases[WeatherName].colors}
+        style={styles.container}>
+            <View style={styles.upper}>
+                <MaterialCommunityIcons
+                    color='#FFFFFF'
+                    size={144}
+                    name={weatherCases[WeatherName].icon}
+                />
+                <Text style={styles.temp}>{temp}°C</Text>
+            </View>
+            <View style={styles.lower}>
+                <Text style={styles.title}>{weatherCases[WeatherName].title}</Text>
+                <Text style={styles.subTitle}>{weatherCases[WeatherName].subTitle}</Text>
+            </View>
+        </LinearGradient>
+    )
+}
+
+export default Weather;
 
 const styles = StyleSheet.create({
     container: {
@@ -59,5 +109,4 @@ const styles = StyleSheet.create({
         marginBottom:24
     }
     
-
 })
